93c644c13114ce7e480a51155d86371d53ec618e
rayles59 <83212748+rayles59@users.noreply.github.com>
2021-11-17 12:43:03 +0100

    image

--- lib/commun_widget/navbotbar.dart
+++ lib/commun_widget/navbotbar.dart
@@ -2,40 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class AppBottomNavigationBar extends StatelessWidget {
-  const AppBottomNavigationBar({Key? key, required this.selected, required this.onTap}) : super(key: key);
-  
-final int selected;
-final Function(int) onTap;
+  const AppBottomNavigationBar(
+      {Key? key, required this.selected, required this.onTap})
+      : super(key: key);
+
+  final int selected;
+  final Function(int) onTap;
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return BottomNavigationBar
-      (
-        onTap: onTap,
-        currentIndex: selected,
-        items:
-         [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text("home")
-            
-          
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.settings),
-            title: new Text("settings")
-            
-          
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.contact_mail),
-            title: new Text("contact")
-            
-          
-          )
-        ]
-    );
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(onTap: onTap, currentIndex: selected, items: [
+      BottomNavigationBarItem(
+          icon: new Icon(Icons.home), title: new Text("home")),
+      BottomNavigationBarItem(
+          icon: new Icon(Icons.settings), title: new Text("settings")),
+      BottomNavigationBarItem(
+          icon: new Icon(Icons.contact_mail), title: new Text("contact"))
+    ]);
   }
-
 }
