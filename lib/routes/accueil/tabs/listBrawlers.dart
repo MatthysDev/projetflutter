b8627fcdfcc1798cdb66e82809525c93e1ebdc65
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-11-17 12:20:12 +0100

    add navbotbar

---
+++ lib/routes/accueil/tabs/listBrawlers.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ListBrawlers extends StatelessWidget {
+  const ListBrawlers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Brawlers');
+  }
+}
