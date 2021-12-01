815228497a7f9d7ce53bb1e0b278f3d0b3fe700d
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-12-01 10:52:34 +0100

    add brawlers in list

--- lib/routes/accueil/tabs/list_brawlers.dart
+++ lib/routes/accueil/tabs/list_brawlers.dart
@@ -1,11 +1,14 @@
+
 import 'package:flutter/widgets.dart';
+import 'package:projetflutter/commun_widget/brawler_for_list.dart';
 
 class ListBrawlers extends StatelessWidget {
   const ListBrawlers({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text('Brawlers');
-    
+print('Test');
+    return Wrap(children: [BrawlerForList(),BrawlerForList(),BrawlerForList(),],);
+
   }
 }
