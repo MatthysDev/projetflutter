4b6d94be28b7e9dcda14fda40d016803bbab4d75
Corentyn <corentyn.masset@gmail.com>
2021-12-01 09:51:16 +0100

    Adding fonts, and icons

---
+++ lib/icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class BrawlIcons {
+  BrawlIcons._();
+
+  static const _kFontFam = 'BrawlIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData star = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData broadsword = IconData(0xe946, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData crownedHeart = IconData(0xe980, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData toolbox = IconData(0xf552, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
--- lib/routes/accueil/tabs/player.dart
+++ lib/routes/accueil/tabs/player.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:projetflutter/icons.dart';
+
 
 class Player extends StatelessWidget {
   const Player({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
     return Container(
       child: Row(
         children:const [
-          Text('data'),
-          Icon(Icons.star),
+          Text('data', style: TextStyle(fontFamily: 'Lilita')),
+          Icon(BrawlIcons.broadsword, size:64),
+          Icon(BrawlIcons.star, size:64),
+          Icon(BrawlIcons.crownedHeart, size:64),
+          Icon(BrawlIcons.toolbox, size:64),
           
         ]
       ),
