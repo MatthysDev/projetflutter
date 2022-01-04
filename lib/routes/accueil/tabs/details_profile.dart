d13ea8852d207f1ceaee13ce6f5c8ee5cd63753a
Gauthier Boddaert <boddaert.gauthier@gmail.com>
2022-01-04 17:42:20 +0100

    detail_profil test

--- lib/routes/accueil/tabs/details_profile.dart
+++ lib/routes/accueil/tabs/details_profile.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:projetflutter/API/profile.dart';
+import 'package:projetflutter/commun_widget/profile_widget.dart';
 import 'package:projetflutter/providers/profile.provider.dart';
 
 class DetailsProfile extends ConsumerWidget {
   const DetailsProfile({Key? key}) : super(key: key);
 
-   @override
+  @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(testProfileProvider).map(
         data: _onData,
@@ -18,15 +19,8 @@ class DetailsProfile extends ConsumerWidget {
           return const Text("loading");
         });
   }
-
 }
 
- Widget _onData(AsyncData<Profile?> data) {
-   if(data.value!.name != null){
-    return Text(data.value!.name,
-                  style: const TextStyle(fontFamily: 'Lilita', fontSize: 20));
- }
- else{
-   return const Text('error');
- }
- }
+Widget _onData(AsyncData<Profile?> data) {
+  return ProfilWidget(profile: data.value!);
+}
